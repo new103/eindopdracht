1248d1b5b809b435d476853e3f6805d9e0438e61
new103 <waligora.t@gmail.com>
2020-11-09 10:06:43 +0100

    09.11.2020

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ let btn = document.querySelector('.toggle-button');
 let menu = document.querySelector('.navbar-links');
 
 btn.addEventListener('click', () => {
-    menu.classList.toggle('on-off');
+  menu.classList.toggle('on-off');
 });
+
+function initMap() {
+  // Your location
+  const loc = {
+    lat: 52.043170,
+    lng: 5.078620
+  };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({
+    position: loc,
+    map: map
+  });
+}
